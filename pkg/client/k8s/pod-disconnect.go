ddb14c0c3d1fd6a94bc0051666c9c41017c6b996
x6a <x6a@7n.io>
2023-11-26 11:12:31 +0000

    fix: node is now removed from database when related k8s pod is deleted

--- pkg/client/k8s/pod-disconnect.go
+++ pkg/client/k8s/pod-disconnect.go
@@ -1,12 +1,14 @@
 package k8s
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/AlecAivazis/survey/v2"
 	"mmesh.dev/m-api-go/grpc/resources/topology"
 	"mmesh.dev/m-cli/pkg/client/k8s/resource"
+	"mmesh.dev/m-cli/pkg/grpc"
 	"mmesh.dev/m-cli/pkg/input"
 	"mmesh.dev/m-cli/pkg/output"
 	"mmesh.dev/m-cli/pkg/status"
@@ -51,6 +53,9 @@ func (api *API) DisconnectPod() {
 
 	s = output.Spinner()
 
+	nxc, grpcConn := grpc.GetTopologyAPIClient()
+	defer grpcConn.Close()
+
 	for _, rID := range selectedIDs {
 		r, ok := resources[rID]
 		if !ok {
@@ -90,6 +95,17 @@ func (api *API) DisconnectPod() {
 				s.Stop()
 				status.Error(err, "Unable to disconnect kubernetes daemonSet")
 			}
+		}
+
+		nr := &topology.NodeReq{
+			AccountID: ni.Node.AccountID,
+			TenantID:  ni.Node.TenantID,
+			NodeID:    ni.Node.NodeID,
+		}
+
+		if _, err := nxc.DeleteNode(context.TODO(), nr); err != nil {
+			s.Stop()
+			status.Error(err, "Unable to delete node for k8s-pod")
 		}
 	}
 
